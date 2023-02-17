950ff2188fc9d0169a854160b7ddf2e3d9f91d62
Wes Hargrove <wesleyhargrove@gmail.com>
2023-02-16 18:23:48 -0700

    Fix tests

--- src/components/Filters/SelectTextFilterOptions/getCheckboxText.test.ts
+++ src/components/Filters/SelectTextFilterOptions/getCheckboxText.test.ts
@@ -2,14 +2,15 @@ import getCheckboxText from "./getCheckboxText";
 
 describe("getCheckboxText", () => {
   const defaultText = "default";
+  const selectedText = "selected";
 
   test("Returns defaultText", () => {
-    const textRep = getCheckboxText({ a: false, b: false }, defaultText);
+    const textRep = getCheckboxText({ a: false, b: false }, defaultText, selectedText);
     expect(textRep).toEqual(defaultText);
   });
 
   test("Returns number of selected options and defaultText", () => {
-    const textRep = getCheckboxText({ a: true, b: false, c: true }, defaultText);
-    expect(textRep).toEqual(`${defaultText} : 2`);
+    const textRep = getCheckboxText({ a: true, b: false, c: true }, defaultText, selectedText);
+    expect(textRep).toEqual(`${selectedText} : 2`);
   });
 });
